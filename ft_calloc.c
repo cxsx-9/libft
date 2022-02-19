44c4d83ba09b0531668b864b17143cc6614abe12
Chaiyapat Santivimol <csantivimol@192.168.0.120>
2022-02-20 02:03:25 +0700

    Finished part I (add calloc and strdup).

---
+++ ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csantivi <csantivi@student.42bangkok.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/20 01:17:09 by csantivi          #+#    #+#             */
+/*   Updated: 2022/02/20 01:30:05 by csantivi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t nitems, size_t size)
+{
+	void	*txt;
+
+	txt = malloc(nitems * size);
+	if (!txt)
+		return (NULL);
+	ft_bzero(txt, nitems);
+	return (txt);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: csantivi <csantivi@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/16 21:52:17 by csantivi          #+#    #+#             */
-/*   Updated: 2022/02/20 00:37:30 by csantivi         ###   ########.fr       */
+/*   Updated: 2022/02/20 01:45:52 by csantivi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,4 +37,7 @@ void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char    *ft_strnstr(const char *s1, const char *s2, size_t n);
 int     ft_atoi(const char *str);
+void	*ft_calloc(size_t nitems, size_t size);
+char    *ft_strdup(const char *s1);
+
 #endif
